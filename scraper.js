04aa5970a66666cfc29b02b2068af401fdd997ac
ASHLEYS-HOME-PC\zacha <zachary.ehle@gmail.com>
2022-02-26 15:15:13 -0500

    Added refresh

--- scraper.js
+++ scraper.js
@@ -1,54 +1,75 @@
-import puppeteer from "puppeteer"
-import https from "https"
-import fs from "fs"
+import puppeteer from "puppeteer";
+import https from "https";
+import fs from "fs";
 
+let page;
+let browser;
 
-const validObjects = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
-'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
-'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard',
-'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
-'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
-'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
-'hair drier', 'toothbrush'];
+const validObjects = ['bicycle', 'car', 'motorcycle', 'bus', 'boat', 'traffic light','fire hydrant', 'a fire hydrant'];
 
+function delay(time) {
+    return new Promise(function(resolve) { 
+        setTimeout(resolve, time)
+    });
+};
 
-(async () => {
-    const browser = await puppeteer.launch({headless:false});
-    const page = await browser.newPage();
+async function start() {
+    browser = await puppeteer.launch({headless:false});
+    page = await browser.newPage();
 
     await page.goto('https://patrickhlauke.github.io/recaptcha/');
 
-    //Initiate reCAPTIA
-    const initCaptia = await page.$("iframe[title='reCAPTCHA']")
-    await initCaptia.click()
+    const initCaptia = await page.$("iframe[title='reCAPTCHA']");
+    await initCaptia.click();
 
-    let challengeFrame 
+    main();
+};
 
-    for (const frame of page.mainFrame().childFrames()){
-        
-        if (frame.url().includes('https://www.google.com/recaptcha/api2/bframe?')){
-            console.log('found the iframe')
-            challengeFrame = frame 
-           
-        }
-    }
+async function refresh(challengeFrame) {
+    await delay(500);
+    const reloadButton = await challengeFrame.$("button[id='recaptcha-reload-button']");
+    await reloadButton.click();
+    await delay(500);
+    main();
+};
 
-    await challengeFrame.waitForSelector('strong')
-    let element = await challengeFrame.$('strong')
-    let objectType = await challengeFrame.evaluate(el => el.textContent, element)
+async function validObject(input, challengeFrame) {
+    if (input !== "bus" && input !== "a fire hydrant") {
+        input = input.slice(0, -1);
+    };
+    if ( validObjects.indexOf(input) > -1 ) {
+        getImage(challengeFrame);
+    } else {
+        refresh(challengeFrame);
+    };
+};
 
-    console.log(objectType)
-    
-    let imagesrc
-    imagesrc = await challengeFrame.$eval('.rc-image-tile-wrapper > img', img => img.src);
-
-    console.log(imagesrc)
+async function getImage(challengeFrame) {   
+    let imagesrc = await challengeFrame.$eval('.rc-image-tile-wrapper > img', img => img.src);
 
     const file = fs.createWriteStream("./images/file.jpg");
     const request = https.get(imagesrc, function(response) {
         response.pipe(file);
     });
+};
 
-})();
+async function main() {
+
+    let challengeFrame;
+
+    for (const frame of page.mainFrame().childFrames()) {
+        
+        if (frame.url().includes('https://www.google.com/recaptcha/api2/bframe?')) {
+            challengeFrame = frame;
+            
+        };
+    };
+
+    await challengeFrame.waitForSelector('strong');
+    let element = await challengeFrame.$('strong');
+    let objectType = await challengeFrame.evaluate(el => el.textContent, element);
+
+    validObject(objectType, challengeFrame);
+};
+
+start();
