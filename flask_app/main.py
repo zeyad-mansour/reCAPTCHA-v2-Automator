cc1c75bf2699ecd0ff966ad6a3e47fc4f759cd51
vlad <zeyadmansour5@gmail.com>
2022-02-26 20:29:33 +0000

    flask stuff

--- flask_app/main.py
+++ flask_app/main.py
@@ -1,43 +1,48 @@
 import os
-import sys
-
-from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+from flask import Flask, request, render_template, flash, redirect, url_for
 from werkzeug.utils import secure_filename
-from PIL import Image
 
 
-
-
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = {'jpg', 'jfif', 'jpeg', 'png'}
 
 app = Flask(__name__)
+app.secret_key = os.urandom(12).hex()
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-app.config['UPLOAD_FOLDER'] = "uploads"
 
-# limit upload size upto 10mb
-app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
+# limit upload size to 3mb
+app.config['MAX_CONTENT_LENGTH'] = 3 * 1024 * 1024
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ('jpg', 'jfif', 'jpeg','png','JPG','JPEG','PNG')
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
-#app = Flask(__name__)
 
 @app.route("/hsiohackathon", methods=['GET', 'POST'])
 def index():
-    if request.method == 'GET':
-        return render_template("index.html")
-
-    f = request.files.getlist("files")
-
-    for file in f:
-        if file.read() == b"" or not allowed_file(file):
-
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
             return render_template("fail.html")
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            return render_template("fail.html")
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template("success.html")
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
-        file_location = app.config['UPLOAD_FOLDER'] + file.filename
-        file.save(file_location)
-
-    return render_template("success.html")
 
 if __name__ == "__main__":
     app.run()
