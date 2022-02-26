c78463c736abaadc9f0686042eca456d1a23d964
vlad <zeyadmansour5@gmail.com>
2022-02-26 18:02:11 +0000

    fsdfasd

--- flask_app/detection/classfier.py
+++ flask_app/detection/classfier.py
@@ -1,1 +1,12 @@
-here
+import torch
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5x.pt', force_reload = True)
+
+img = 'test_images/bicycles.jfif'
+results = model(img)
+
+results.pandas().xyxy[0]
+
+
+def captcha_classifier(class_type, image_path):
+    pass #todo
