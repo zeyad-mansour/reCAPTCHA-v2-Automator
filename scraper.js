3eb7cbb87fda5238eebb9ef3491ae31c636bb16c
ASHLEYS-HOME-PC\zacha <zachary.ehle@gmail.com>
2022-02-26 13:04:59 -0500

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -2,7 +2,8 @@ const request = require("request-promise");
 const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
 
-const cache = {};
+const validObjects = {};
+
 
 (async () => {
     const browser = await puppeteer.launch({headless:false});
@@ -11,10 +12,40 @@ const cache = {};
     await page.goto('https://patrickhlauke.github.io/recaptcha/');
 
     //Initiate reCAPTIA
-    await page.click("iframe[title='reCAPTCHA']")
+    const initCaptia = await page.$("iframe[title='reCAPTCHA']")
+    await initCaptia.click()
 
+    let challengeFrame 
+
+    for (const frame of page.mainFrame().childFrames()){
+        
+        if (frame.url().includes('https://www.google.com/recaptcha/api2/bframe?')){
+            console.log('found the iframe')
+            challengeFrame = frame 
+           
+        }
+    }
+
+    await challengeFrame.waitForSelector('strong')
+    let element = await challengeFrame.$('strong')
+    let objectType = await challengeFrame.evaluate(el => el.textContent, element)
+
+    console.log(objectType)
+    let size
+    let imagesrc
+
+    if (await challengeFrame.$('rc-image-title-33') !== null) {
+        imagesrc = await challengeFrame.$$eval('.rc-image-title-33[src]');
+        size = 300
+    } else {
+        imagesrc = await challengeFrame.$$eval('.rc-image-title-44[src]');
+        size = 450
+    }
+   
+    console.log(imagesrc)
     
 
-  })();
+
+})();
 
   
