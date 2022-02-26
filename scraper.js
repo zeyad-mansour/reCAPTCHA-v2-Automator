6636522179b9e34bf376a208c8fc0da1ff8ab571
ASHLEYS-HOME-PC\zacha <zachary.ehle@gmail.com>
2022-02-26 16:58:35 -0500

    FInished ree

--- scraper.js
+++ scraper.js
@@ -1,11 +1,15 @@
 import puppeteer from "puppeteer";
 import https from "https";
 import fs from "fs";
+import { randomInt } from "crypto";
 
 let page;
 let browser;
 
-const validObjects = ['bicycle', 'car', 'motorcycle', 'bus', 'boat', 'traffic light','fire hydrant', 'a fire hydrant'];
+const validObjects = ['bicycle', 'car', 'motorcycle', 'bus', 'boat', 'traffic light','fire hydrant'];
+
+let input = null;
+let array;
 
 function delay(time) {
     return new Promise(function(resolve) { 
@@ -25,6 +29,7 @@ async function start() {
     main();
 };
 
+
 async function refresh(challengeFrame) {
     await delay(500);
     const reloadButton = await challengeFrame.$("button[id='recaptcha-reload-button']");
@@ -34,23 +39,27 @@ async function refresh(challengeFrame) {
 };
 
 async function validObject(input, challengeFrame) {
-    if (input !== "bus" && input !== "a fire hydrant") {
+    if (input !== "bus") {
         input = input.slice(0, -1);
     };
     if ( validObjects.indexOf(input) > -1 ) {
-        getImage(challengeFrame);
+        getImage(challengeFrame, input);
     } else {
         refresh(challengeFrame);
     };
 };
 
-async function getImage(challengeFrame) {   
+async function getImage(challengeFrame, type) {   
     let imagesrc = await challengeFrame.$eval('.rc-image-tile-wrapper > img', img => img.src);
 
-    const file = fs.createWriteStream("./images/file.jpg");
+    const fileName = "./images/" + String(type) + ".jpg"
+    const fileWrite = fs.createWriteStream(fileName);
     const request = https.get(imagesrc, function(response) {
-        response.pipe(file);
+        response.pipe(fileWrite);
     });
+
+    getResponse();
+    
 };
 
 async function main() {
@@ -70,6 +79,82 @@ async function main() {
     let objectType = await challengeFrame.evaluate(el => el.textContent, element);
 
     validObject(objectType, challengeFrame);
+};
+
+//Solver Functions
+
+async function solve4x4(array) {
+    let challengeFrame;
+
+    for (const frame of page.mainFrame().childFrames()) {
+        
+        if (frame.url().includes('https://www.google.com/recaptcha/api2/bframe?')) {
+            challengeFrame = frame;
+        };
+    };
+
+    let table = await challengeFrame.waitForSelector('tbody');
+    let cel;
+
+    for (var i = 0; i <  16; i++) {
+        cel = await challengeFrame.$("td[tabindex='" + String(i + 4) + "']");
+        if (array[i] == 1) {
+            await cel.click();
+            await delay(randomInt(300,700))
+        };
+    };
+    if (input !== "0000000000000000") main();
+    if (input == "0000000000000000") {
+        let verify = await challengeFrame.$("button[id='recaptcha-verify-button']");
+        await verify.click();
+    }
+};
+
+
+async function solve3x3(array) {
+    let challengeFrame;
+
+    for (const frame of page.mainFrame().childFrames()) {
+        
+        if (frame.url().includes('https://www.google.com/recaptcha/api2/bframe?')) {
+            challengeFrame = frame;
+        };
+    };
+
+    let table = await challengeFrame.waitForSelector('tbody');
+    let cel;
+
+    for (var i = 0; i < 9; i++) {
+        cel = await challengeFrame.$("td[tabindex='" + String(i + 4) + "']");
+        if (array[i] == 1) {
+            await cel.click();
+            await delay(randomInt(300,700))
+        };
+    };
+    if (input !== "000000000") main();
+    if (input == "000000000") {
+        let verify = await challengeFrame.$("button[id='recaptcha-verify-button']");
+        await verify.click();
+    }
+};
+
+function solve() {
+    if (input.length == 9) {
+        solve3x3(array);
+    } else {
+        solve4x4(array);
+    };
+};
+
+function getResponse() {
+    fs.readFile('answer.txt', "utf8", function(err, data) {
+        if (data == null) getResponse;
+        if (data !== null) {
+            input = data;
+            array = input.slice('');
+            solve();
+        }
+    });
 };
 
 start();
